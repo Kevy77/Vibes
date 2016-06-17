2d5c1d9f6dda5bd4c21f376791c9568c9bc53146
Kevy77 <kevy@hkcreativity.com>
2016-06-17 13:26:37 +0200

    //profile_fini

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,35 @@
-$(document).ready(function(){
-  $(".search_bar").hide();
-  $("aside.left").hide();
-  $('#search').click(function(e){
-          e.preventDefault();
-          $(".search_bar").fadeToggle(600);
-      });
-  $('.hamburger').click(function(e){
-          e.preventDefault();
-          $(".full_page").toggleClass('slide');
-          $(".hover_ap").toggleClass('all__');
-          $("aside.left").fadeToggle(600);
-      });
+$(document).ready(function() {
+    $(".search_bar").hide();
+    $("aside.left").hide();
+    $("#follow_list").hide();
+    $("#coupcoeur").hide();
+    $('#search').click(function(e) {
+        e.preventDefault();
+        $(".search_bar").fadeToggle(600);
+    });
+    $('.hamburger').click(function(e) {
+        e.preventDefault();
+        $(".full_page").toggleClass('slide');
+        $(".hover_ap").toggleClass('all__');
+        $("aside.left").fadeToggle(600);
+    });
+    $('#heart_btn').click(function(e) {
+        e.preventDefault();
+        $("#musique").slideUp(600);
+        $("#follow_list").slideUp(600);
+        $("#coupcoeur").fadeIn(600);
+    });
+    $('#follow_btn').click(function(e) {
+        e.preventDefault();
+        $("#musique").slideUp(600);
+        $("#coupcoeur").slideUp(600);
+        $("#follow_list").fadeIn(600);
+    });
+    $('#home_btn').click(function(e) {
+        e.preventDefault();
+        $("#musique").slideUp(600);
+        $("#coupcoeur").slideUp(600);
+        $("#follow_list").slideUp(600);
+        $("#musique").fadeIn(600);
+    });
 });
